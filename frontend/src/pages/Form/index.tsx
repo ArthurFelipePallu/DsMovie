b1fc544df1748a85fb6cb3b6fbbbaf390afa5174
Arthur Pallu <arthurfelipepallu@gmail.com>
2022-05-09 18:42:40 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,6 @@
+function Form(){
+    return(
+        <h1>pag Formulário</h1>
+    );
+}
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,6 @@
+function Listing(){
+    return(
+        <h1>pag listagem</h1>
+    );
+}
+export default Listing;
